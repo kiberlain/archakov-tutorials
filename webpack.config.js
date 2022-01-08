9f39b683dcf289cc6d9959060c8b57c82f2c3825
kiberlain <kiberlain@gmail.com>
2022-01-08 21:02:34 +0300

    домашняя работа 3

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,13 @@ module.exports = {
                 test: /\.(css)$/,
                 use: ["style-loader","css-loader","sass-loader"],
             },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "images/[name][hash][ext]",
+                },
+            },
         ],
     },
     resolve: { extensions: ["*", ".js", ".jsx"] },
